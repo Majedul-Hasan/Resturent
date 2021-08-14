c2a9d167c8a2bd3c2d3df27372e8b2760afff168
Majedul Hasan <77298488+Majedul-Hasan@users.noreply.github.com>
2021-08-14 18:52:22 +0600

    static responsiv singlepage website

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector(".menu");
+const targets = document.querySelectorAll(".target");
+
+menuBtn.addEventListener("click", () => {
+  targets.forEach((target) => {
+    target.classList.toggle("change");
+  });
+});
+
+const icons = document.querySelectorAll(".section-1-icons i");
+
+let i = 1;
+
+setInterval(() => {
+  i++;
+  const icon = document.querySelector(".section-1-icons .change");
+
+  icon.classList.remove("change");
+  if (i > icons.length) {
+    icons[0].classList.add("change");
+    i = 1;
+  }
+
+  icon.nextElementSibling.classList.add("change");
+}, 2000);
